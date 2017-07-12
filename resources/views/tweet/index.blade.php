d0753d18e7122e78cbf2669dc646138a93189cf0
Tom Holland <codesman@hackattic.net>
2017-07-12 10:28:25 -0700

    Tweet index content

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -2,13 +2,39 @@
 
 @section('content')
     <div class="container">
+        <div class="row mb-1">
+            <div class="col-md-12">
+                <button>Add A Tweet</button>
+                <button>Upload Tweets</button>
+                <button>Schedule Tweets</button>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Tweet Index</div>
+                    <div class="panel-heading">Tweet List</div>
 
                     <div class="panel-body">
-                        There are no Tweets
+                        @if (!empty($tweets))
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>{{ $table_headers['subject'] }}</th>
+                                    <th>{{ $table_headers['url'] }}</th>
+                                    <th>{{ $table_headers['image'] }}</th>
+                                </tr>
+                                </thead>
+                                <tr>
+                                    @foreach($tweets as $tweet)
+                                        <td>{{ $tweet->subject }}</td>
+                                        <td><a href="{{ $tweet->url }}">{{ $tweet->url }}</a></td>
+                                        <td><a href=""><img src="http://placehold.it/22/22"></a></td>
+                                    @endforeach
+                                </tr>
+                            </table>
+                        @else
+                            No Tweets :(
+                        @endif
                     </div>
                 </div>
             </div>
